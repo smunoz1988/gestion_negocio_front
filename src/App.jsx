67d31ff4f771ed7c4f404ae7575466d2f5802767
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-19 21:24:39 -0500

    setup packages and import then on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import './App.css';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React, { useState} from 'react';
+import DatePicker from 'react-datepicker';
+
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
     </div>
   )
 }
