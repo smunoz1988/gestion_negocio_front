700683cf312f0f8eaddac5579f1ec553dade1dce
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-27 13:32:32 -0800

    setup linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
+);
 
-export default App
+export default App;
