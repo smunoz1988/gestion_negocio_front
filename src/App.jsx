2576e437b489e69fbbf75141668e97a94fc48e2e
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-27 15:52:16 -0800

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => (
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NormalRoute from './components/NormalRoute';
+import ProtectedRoute from './components/ProtectedRoute';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+import Navbar from './components/NavBar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<NormalRoute />} />
+          <Route exact path='/protected_route' element={<ProtectedRoute />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
