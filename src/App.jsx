5be3ecdaf220be058112ac912e3a5374fb028dfa
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-12-28 18:38:40 -0800

    set user registration routes and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
-import WithAuth from './components/auth/WithAuth';
+import { checkAuth } from './actions/auth';
+
 
 const App = () => {
+  const dispatch = useDispatch();
+ 
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
   return (
     <Router>
       <Routes>
         <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path="/protected" element={<WithAuth(ProtectedRoute) />} />
+        <Route path='/' element={<Login />} />
+        <Route path="/protected" element={<ProtectedRoute />} />
         <Route path='*' element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Navbar from './Navbar'
+import Login from './auth/Login'
 
-const ProtectedRoute = () => (
-    <div>
-        <Navbar />
-        <h1>Protected Route</h1>
-        <p>aca va la aplicacion</p>
-    </div>
-)
+const ProtectedRoute = ({ authChecked, loggedIn, currentUser }) => {
+  const isAuth = useSelector(state => state.auth.loggedIn);
+  const user = useSelector(state => state.auth.currentUser);
+  return isAuth ? (
+    <>
+      <Navbar currentUser={currentUser} />
+      <p>Hello, {user.email}</p>
+      <h1>Full Calendar</h1>
+    </>
+  ) : (
+    <>
+      <p>Please, login</p>
+      <Login />
+    </>
+    
+  )
+}
 
 export default ProtectedRoute
