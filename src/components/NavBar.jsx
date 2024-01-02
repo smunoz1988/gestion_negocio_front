57a4072d225d6fd3978ab8dd227f701671273eb1
Daniela Moreno <dani9292@hotmail.com>
2024-01-02 13:05:36 -0500

    add basic layout to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Logout from './auth/Logout';
+import '../styles/navbar.css';
 
 const Navbar = () => {
   const user = useSelector((state) => state.auth.currentUser);
   return (
-    <div>
+    <div className="navbar-top">
       <p>nombre de la empresa</p>
       <p>
         Hola,
